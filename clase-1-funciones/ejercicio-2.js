c268f7ab75e4469085348e0ddf7938235abfe630
Victor Ramirez <vicdeveloper83@gmail.com>
2021-03-10 17:19:26 +0000

    second

--- clase-1-funciones/ejercicio-2.js
+++ clase-1-funciones/ejercicio-2.js
@@ -2,6 +2,14 @@
 // 2. Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
 // 3. Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].  
 // 4. Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
+// --------victor first---------
 const str1= prompt('escribe tu texto aqui')
-let strCounter = ()=> {return str1.length} 
-console.log(strCounter())
+/*let strCounter = ()=> {return str1.length} 
+console.log(strCounter())*/
+
+// -------------victor second-----------
+/*const num= 4
+let strNum= ()=> { return str1.substring(0,num)}
+console.log(strNum())*/
+
+//-------------victor third------------
